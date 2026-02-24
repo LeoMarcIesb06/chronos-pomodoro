f1df326ec167f1ed33acf507a83f623536702273
Seu NomeMarcelo <marcelo.v.soares@iesb.edu.br>
2026-02-24 10:42:48 -0300

    Logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import { Heading } from './components/Heading';
-
 import './styles/theme.css';
 import './styles/global.css';
-import { TimerIcon } from 'lucide-react';
-import { Container } from './components/Container';
+
+import { Heading } from './components/Heading/Heading';
+import { Container } from './components/Container/Container';
+import { Logo } from './components/Logo/Logo';
 
 export function App() {
   return (
     <>
       <Container>
-        <Heading>
-          Chronos Pomodoro
-          <TimerIcon size={32} />
-        </Heading>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
-          excepturi magnam, distinctio sint aliquam eos reprehenderit repellat
-          eum corporis adipisci? Iste incidunt sapiente ipsa atque distinctio
-          dignissimos nisi! Reiciendis, quis.
-        </p>
+        <Logo />
+      </Container>
+
+      <Container>
+        <Heading>MENU</Heading>
       </Container>
     </>
   );
